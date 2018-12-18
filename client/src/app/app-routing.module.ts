3b691f078b8fa416ee23e9179a35871e705d56f9
nik4un <nik4un@yandex.ru>
2018-12-18 12:15:24 +0300

    Добавление страниц

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { AuthGuard } from './shared/classes/auth.guard';
 import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { AnalyticsPageComponent } from './analytics-page/analytics-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { CategoriesPageComponent } from './categories-page/categories-page.component';
 
 const routes: Routes = [
   {
@@ -18,7 +22,11 @@ const routes: Routes = [
   },
   {
     path: '', component: SiteLayoutComponent, canActivate: [AuthGuard], children: [
-      { path: 'overview', component: OverviewPageComponent}
+      { path: 'overview', component: OverviewPageComponent},
+      { path: 'analytics', component: AnalyticsPageComponent},
+      { path: 'history', component: HistoryPageComponent},
+      { path: 'order', component: OrderPageComponent},
+      { path: 'categories', component: CategoriesPageComponent},
     ]
   }
 ];
