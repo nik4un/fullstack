5e43d9d0f846355be7aec4bbcc51c69beca2377b
nik4un <nik4un@yandex.ru>
2019-01-09 18:04:20 +0300

    History page. Детали заказа

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,18 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  ViewChild
+} from '@angular/core';
 import { Order } from '../../shared/interfaces';
+import { MaterialService, ModalInstance } from '../../shared/classes/material.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.css']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements AfterViewInit, OnDestroy {
 
   @Input() orders: Order[];
+  @ViewChild('modal') modalRef: ElementRef;
+  modal: ModalInstance;
+  selectedOrder: Order;
 
-  constructor() { }
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
 
-  ngOnInit() {
+  ngOnDestroy(): void {
+    this.modal.destroy();
+  }
+
+  computePrice(order: Order): number {
+    return order.list.reduce((acc, el) => acc + el.cost * el.quantity, 0);
+  }
+
+  onSelectOrder(order) {
+    this.selectedOrder = order;
+    this.modal.open();
+  }
+
+  onCancel() {
+    this.modal.close();
   }
 
 }
