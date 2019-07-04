fc8d75955e74e360a9be44fc4f7d74e836d610dd
nik4un <nik4un@yandex.ru>
2019-07-04 16:39:05 +0300

    Add loading environment variables from a .env file into process.env

--- config/keys.prod.js
+++ config/keys.prod.js
@@ -1,6 +1,6 @@
-// для продакшен будем брать из консольного ввода
+// для продакшен будем брать значения, помещаемые в process.env из файла .env
 module.exports = {
-  serverPort: process.env.PORT || 3333,
+  serverPort: process.env.PORT || 3000,
   mongoURI: process.env.MONGO_URI,
   cert: process.env.JWT, // сертификат для token
 };
